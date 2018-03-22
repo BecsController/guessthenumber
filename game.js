0211137dec2b957d780571f64b3024efaee06c81
Rebecca Dugdale <bec.dugdale16@gmail.com>
2018-03-22 22:41:12 +1300

    Reset working

--- game.js
+++ game.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', start)
 
 var generateNumber = Math.floor(Math.random() * 20) + 1;
 
+
 function start() {
 
 var answer = document.getElementById("answer");
@@ -9,30 +10,33 @@ var submit = document.getElementById("sub");
 var reset = document.getElementById("reset");
 
 submit.addEventListener("click", compare);
-submit.addEventListener("click", reset);
+reset.addEventListener("click", resetGame);
 
 function compare(){
 
   alert(generateNumber);
-  var guess = document.forms["guessForm"]["fnumber"].value;
+var guess = document.forms["guessForm"]["fnumber"].value;
     alert(guess);
 
 if (guess < generateNumber){
   answer.innerHTML = "It's a bit higher actually";
 } else if (guess >= 21) {
   answer.innerHTML = "Um, 1 - 20 innit?";
-} else if (guess <= 0){
+} else if (guess < 1){
   answer.innerHTML = "Um, 1 - 20 innit?";
 } else if (guess > generateNumber){
   answer.innerHTML = "It's a bit lower actually";
 } else if (guess == generateNumber) {
   answer.innerHTML = "Spot on! You're psychic!";
   reset.classList.toggle("invisible");
-  document.forms["guessForm"]["fnumber"].value = "";
-    }
-  }
 }
+  }; //end compare function
 
-//reset.addEventListener("click" function(){
-//  reset.classList.toggle("invisible");
-//})
+}; //end start function
+
+function resetGame(){
+  reset.classList.toggle("invisible");
+  var generateNumber = Math.floor(Math.random() * 20) + 1;
+  document.forms["guessForm"]["fnumber"].value = "";
+  start()
+};
