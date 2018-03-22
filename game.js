6028bc27ec2ade58eb7c37ddda2b9296574fe79c
Rebecca Dugdale <bec.dugdale16@gmail.com>
2018-03-22 20:17:22 +1300

    reset button add

--- game.js
+++ game.js
@@ -4,9 +4,9 @@ var generateNumber = Math.floor(Math.random() * 20);
 
 function start() {
 
-var guess = document.getElementById("guess").value;
 var answer = document.getElementById("answer");
 var submit = document.getElementById("sub");
+var reset = document.getElementById("reset");
 
 submit.addEventListener("click", function(){
   alert(generateNumber);
@@ -14,14 +14,16 @@ submit.addEventListener("click", function(){
     alert(guess);
 if (guess < generateNumber){
   answer.innerHTML = "It's a bit higher actually";
-} else if (guess >= 21 || guess <= 0) {
+} else if (guess >= 21) {
+  answer.innerHTML = "Um, 1 - 20 innit?";
+} else if (guess <= 0.0){
   answer.innerHTML = "Um, 1 - 20 innit?";
 } else if (guess > generateNumber){
   answer.innerHTML = "It's a bit lower actually";
 } else if (guess == generateNumber) {
   answer.innerHTML = "Spot on! You're psychic!";
+  reset.classList.toggle('invisible');
 }
-
   })
 
 }
