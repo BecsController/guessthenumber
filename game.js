edc037f12f20f5eb49205f0a1f925247d1d4cdb8
Rebecca Dugdale <bec.dugdale16@gmail.com>
2018-03-22 19:58:29 +1300

    fix to guess value retrieve

--- game.js
+++ game.js
@@ -1,13 +1,27 @@
 document.addEventListener('DOMContentLoaded', start)
 
+var generateNumber = Math.floor(Math.random() * 20);
+
 function start() {
 
-var generateNumber = Math.floor(Math.random() * 20);
-var guess = document.getElementById("guess");
+var guess = document.getElementById("guess").value;
 var answer = document.getElementById("answer");
 var submit = document.getElementById("sub");
 
 submit.addEventListener("click", function(){
+  alert(generateNumber);
+  var guess = document.forms["guessForm"]["fnumber"].value;
+    alert(guess);
+if (guess < generateNumber){
   answer.innerHTML = "It's a bit higher actually";
-})
+} else if (guess >= 21 || guess <= 0) {
+  answer.innerHTML = "Um, 1 - 20 innit?";
+} else if (guess > generateNumber){
+  answer.innerHTML = "It's a bit lower actually";
+} else if (guess == generateNumber) {
+  answer.innerHTML = "Spot on! You're psychic!";
+}
+
+  })
+
 }
