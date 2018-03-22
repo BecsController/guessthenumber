b5aaa533ef33c66c5c537e8ed7cc623a884f7658
Rebecca Dugdale <bec.dugdale16@gmail.com>
2018-03-22 14:25:11 +1300

    click add and number generate

---
+++ game.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', guess)
+
+function guess() {
+    addEventListener()
+}
+function addEventListener() {
+
+var button = document.getElementByClassName("sub")
+    button.addEventListener("click", consider)
+}
+
+var generateNumber = Math.floor(Math.random() * 20);
+
+function consider(){
+
+}
