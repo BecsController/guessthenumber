850944ca90df42aa07ccfb39bf6ccf410e161c2b
Rebecca Dugdale <bec.dugdale16@gmail.com>
2018-03-23 10:40:09 +1300

    color class toggle

--- game.js
+++ game.js
@@ -16,14 +16,19 @@ function compare(){
 
   if (guess < generateNumber){
       answer.innerHTML = "It's a bit higher actually";
+      answer.classList.toggle('red','orange','green', 'purple');
     } else if (guess >= 21) {
       answer.innerHTML = "Um, 1 - 20 innit?";
-    } else if (guess < 1){
-      answer.innerHTML = "Um, 1 - 20 innit?";
+      answer.classList.toggle('orange','blue','green','purple','red');
+    } else if (guess.length == 0){
+      answer.innerHTML = "Aw, c,mon guess!";
+      answer.classList.toggle('blue','green','purple','red','orange');
     } else if (guess > generateNumber){
       answer.innerHTML = "It's a bit lower actually";
+      answer.classList.toggle('green','purple','red','orange','blue');
     } else if (guess == generateNumber) {
       answer.innerHTML = "Spot on! You're psychic!";
+      answer.classList.toggle('purple','red','orange','blue','green');
       reset.classList.toggle("invisible"); //toggle button on
     }
   }; //end compare function
