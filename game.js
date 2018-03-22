886b3a9701873303152a75391efc623f190fc19e
Rebecca Dugdale <bec.dugdale16@gmail.com>
2018-03-22 16:30:20 +1300

    initial click change

--- game.js
+++ game.js
@@ -2,11 +2,12 @@ document.addEventListener('DOMContentLoaded', start)
 
 function start() {
 
-var submit = document.getElementById("sub");
 var generateNumber = Math.floor(Math.random() * 20);
 var guess = document.getElementById("guess");
-var answer = document.getElementsByClassName("answer");
+var answer = document.getElementById("answer");
+var submit = document.getElementById("sub");
 
-
-
+submit.addEventListener("click", function(){
+  answer.innerHTML = "It's a bit higher actually";
+})
 }
